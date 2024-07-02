052206ee6751750fd46a3afb723d0ac1ed08197a
KekiGC <eduardoeg002@gmail.com>
2024-07-01 23:56:09 -0400

    xd

--- server/dist/app.js
+++ server/dist/app.js
@@ -14,6 +14,8 @@ const special_routes_1 = __importDefault(require("./routes/special.routes"));
 const patient_routes_1 = __importDefault(require("./routes/patient.routes"));
 const medicalRecord_routes_1 = __importDefault(require("./routes/medicalRecord.routes"));
 const appointment_routes_1 = __importDefault(require("./routes/appointment.routes"));
+//import pdfroutes from "./routes/pdf.routes";
+const medicalRest_routes_1 = __importDefault(require("./routes/medicalRest.routes"));
 const invoice_routes_1 = __importDefault(require("./routes/invoice.routes"));
 //inicio
 const app = (0, express_1.default)();
@@ -36,5 +38,7 @@ app.use(special_routes_1.default);
 app.use(patient_routes_1.default);
 app.use(medicalRecord_routes_1.default);
 app.use(appointment_routes_1.default);
+//app.use(pdfroutes);
+app.use(medicalRest_routes_1.default);
 app.use(invoice_routes_1.default);
 exports.default = app;
